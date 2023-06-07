23142304f6e1cabd8d2dec66137b58cc432c77a2
AdeFuaad <adesholafuaad@gmail.com>
2023-06-06 21:55:08 -0400

    server URL updated

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   try {
-    const response = await fetch("http://localhost:3000", {
+    const response = await fetch("https://me-q4yy.onrender.com/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
